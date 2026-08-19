a0af4946e9aea4f1805d5d794c46de48b909ddb2
Daniel <justus.daniel@web.de>
2026-08-19 19:57:41 +0200

    refactor(desktop-core): move Trash Management into DeskCoreTrash.swift

--- Sources/SymDeskCore/DeskCore.swift
+++ Sources/SymDeskCore/DeskCore.swift
@@ -1033,24 +1033,6 @@ public final class DeskCore: ObservableObject {
         }
     }
 
-    // MARK: - Trash Management
-
-    public func trashList() async throws -> [TrashEntry] {
-        try await runDecoding([TrashEntry].self, arguments: ["trash", "list", "--json"] + vaultArgs)
-    }
-
-    public func trashRestore(name: String) async throws {
-        _ = try await runChecked(arguments: ["trash", "restore", name, "--json"] + vaultArgs)
-    }
-
-    public func trashPurgeAll() async throws {
-        _ = try await runChecked(arguments: ["trash", "purge", "--all", "--json"] + vaultArgs)
-    }
-
-    public func noteDelete(path: String) async throws {
-        _ = try await runChecked(arguments: ["delete", path, "--json"] + vaultArgs)
-    }
-
     // MARK: - Consume Folder
 
     /// Status of the consume (watched inbox) folder.

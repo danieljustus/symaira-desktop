c487175e0b3cb30d8cf5cd6599b5552c76b68963
Daniel <justus.daniel@web.de>
2026-09-24 21:15:19 +0200

    test(dataset): record Windows purge retry error

--- internal/service/port_dataset_purge_contract_test.go
+++ internal/service/port_dataset_purge_contract_test.go
@@ -42,6 +42,7 @@ type datasetPurgeRecoveryFixtureCase struct {
 	Before       datasetPurgeSnapshot `json:"before"`
 	After        datasetPurgeSnapshot `json:"after"`
 	Error        string               `json:"error,omitempty"`
+	WindowsError string               `json:"windows_error,omitempty"`
 }
 
 type datasetPurgeSnapshot struct {
@@ -323,6 +324,7 @@ func datasetPurgeReplacementTrashRetryCase(t *testing.T) datasetPurgeRecoveryFix
 		Before:       before,
 		After:        after,
 		Error:        "content changed",
+		WindowsError: "was replaced",
 	}
 }
 
